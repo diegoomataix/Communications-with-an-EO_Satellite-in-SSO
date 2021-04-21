0d9d52720befb0377dc7cd2f93bd7401a900758b
Andrea <andreabrv98@gmail.com>
2021-04-21 14:06:17 +0200

    Same shit w/ some changes

--- Scripts & Data/CN.m
+++ Scripts & Data/CN.m
@@ -5,6 +5,10 @@ load('Elevation10_10s'); load('Elevation20_10s'); load('Elevation30_10s')
 d10 = 1e3*R_Elevation10_10s(1:27,2);      % [m]
 d20 = 1e3*R_Elevation20_10s(1:46,2);      % [m]
 d30 = 1e3*R_Elevation30_10s(1:33,2);      % [m]
+
+t10 = R_Elevation10_10s(1:27,1)-R_Elevation10_10s(1,1);      % [m]
+t20 = R_Elevation20_10s(1:46,1)-R_Elevation20_10s(1,1);      % [m]
+t30 = R_Elevation30_10s(1:33,1)-R_Elevation30_10s(1,1);      % [m]
 %% Compute/Read Access Time Data
 load('t_Elevation10'); load('t_Elevation20'); load('t_Elevation30')
 %% DATOS
@@ -29,36 +33,36 @@ Lfs10 = 20*log10(4*pi*d10/lambda);
 Lfs20 = 20*log10(4*pi*d20/lambda);
 Lfs30 = 20*log10(4*pi*d30/lambda);
 
-t10 = linspace(0,t_Elevation10(1,1),size(Lfs10,1));
-t20 = linspace(0,t_Elevation20(1,1),size(Lfs20,1));
-t30 = linspace(0,t_Elevation30(1,1),size(Lfs30,1));
+% t10 = linspace(0,t_Elevation10(1,1),size(Lfs10,1));
+% t20 = linspace(0,t_Elevation20(1,1),size(Lfs20,1));
+% t30 = linspace(0,t_Elevation30(1,1),size(Lfs30,1));
 
 C_N_10 = EIRP + G_T - Lfs10 - L_X - k -10*log10(B);
 C_N_20 = EIRP + G_T - Lfs20 - L_X - k -10*log10(B);
 C_N_30 = EIRP + G_T - Lfs30 - L_X - k -10*log10(B);
 
-% plotCN(t10,d10,C_N_10); plotCN(t20,d20,C_N_20); plotCN(t30,d30,C_N_30)
+plotCN(t10,d10,C_N_10,10); plotCN(t20,d20,C_N_20,20); plotCN(t30,d30,C_N_30,30)
 
 %% TASK 4
 
-CNreq = [16.13, 15.83, 14.41, 13.83, 12.90, 13.08, 11.77, 11.19, ...
+CNreq = [16.13, 15.83, 14.41, 13.83, 12.90, 12.08, 11.77, 11.19, ...
     10.35, 9.08, 8.05, 6.68, 5.63, 5.34, 4.82, 4.21, 3.23, 2.4, ...
     1,14, -0.25, -1.3, -2.54];
 
 CNreq = CNreq';
 
-MODCODs = ['32APSK 9/10', '32APSK 8/9', '32APSK 5/6 ', '32APSK 4/5',...
-    '32APSK 3/4', '16APSK 8/9', '16APSK 5/6 ', '16APSK 4/5', '16APSK 3/4', ...
-    '16APSK 2/3', '8PSK 3/4', '8PSK 2/3', '8PSK 3/5 ', 'QPSK 5/6', ...
-    'QPSK 4/5', 'QPSK 3/4', 'QPSK 2/3', 'QPSK 3/5', 'QPSK 1/2', ...
-    'QPSK 2/5', 'QPSK 1/3', 'QPSK 1/4'];
+% MODCODs = ['32APSK 9/10', '32APSK 8/9', '32APSK 5/6 ', '32APSK 4/5',...
+%     '32APSK 3/4', '16APSK 8/9', '16APSK 5/6 ', '16APSK 4/5', '16APSK 3/4', ...
+%     '16APSK 2/3', '8PSK 3/4', '8PSK 2/3', '8PSK 3/5 ', 'QPSK 5/6', ...
+%     'QPSK 4/5', 'QPSK 3/4', 'QPSK 2/3', 'QPSK 3/5', 'QPSK 1/2', ...
+%     'QPSK 2/5', 'QPSK 1/3', 'QPSK 1/4'];
 
-plotMODCOD(C_N_10,t10,CNreq)
-plotMODCOD(C_N_20,t20,CNreq)
-plotMODCOD(C_N_30,t30,CNreq)
+plotMODCOD(C_N_10,t10,CNreq,10)
+plotMODCOD(C_N_20,t20,CNreq,20)
+plotMODCOD(C_N_30,t30,CNreq,30)
 
 %% FUNCTIONS
-function plotCN(t,d, C_N)
+function plotCN(t,d, C_N, elev)
 figure()
 hold on
 box on
@@ -71,10 +75,11 @@ plot(t,C_N)
 yyaxis right
 %ylabel("\it d [m]",0,'HorizontalAlignment','right','Position',[120.4 max(TT)])
 plot(t,d)
+title(['Elevación ' num2str(elev) 'º'])
 hold off
 end
 
-function plotMODCOD(CNlink,t,CNreq)
+function plotMODCOD(CNlink,t,CNreq,elev)
 cont=0;
 pos = zeros(length(CNlink),1);
 
@@ -88,7 +93,6 @@ for i = 1:length(CNlink)
     end
 end
 
-cont=1;
 pos(1:3) = max(pos(1:3));
 for i = 4:length(CNlink)-3
     if pos(i) ~= pos(i-1)
@@ -106,11 +110,18 @@ for i = 1:length(pos)
     MODCOD(i) = CNreq(pos(i))';
 end
 
+%Intento de evitar que el pico se salga por el lado
+
+% t_fine = linspace(0,t(end),50);
+% MODCODfine = interp1(t, MODCOD, t_fine);
+% CNlink_fine = interp1(t, CNlink, t_fine);
+
 figure()
 hold on
 stairs(t,MODCOD)
 set(gca,'linewidth',0.75)
 set(gca,'fontsize',14)
 plot(t,CNlink)
+title(['Elevación ' num2str(elev) 'º'])
 hold off
 end
